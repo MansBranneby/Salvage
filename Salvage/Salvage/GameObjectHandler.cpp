0550f967131061360e2869e2ed140c9693ad4a82
Måns Bränneby <mansbranneby@hotmail.com>
2019-06-18 19:40:30 +0200

    Building structure of GameObjectHandler using a double pointer over vector for speed reasons.

--- Salvage/Salvage/GameObjectHandler.cpp
+++ Salvage/Salvage/GameObjectHandler.cpp
@@ -1,9 +1,49 @@
 #include "GameObjectHandler.h"
 
-GameObjectHandler::GameObjectHandler()
+GameObjectHandler::GameObjectHandler(int capacity)
+{
+	_nrOfObjects = 0;
+	_capacity = capacity;
+	_gameObjects = new GameObject*[_capacity];
+	initiate();
+}
+
+void GameObjectHandler::freeMemory()
+{
+	for (size_t i = 0; i < _nrOfObjects; i++)
+		delete _gameObjects[i];
+	delete[] _gameObjects;
+}
+
+void GameObjectHandler::expand()
+{
+	//Expanding double pointer holding gameObjects
+	_capacity += 10;
+	GameObject** temp = new GameObject*[_capacity];
+
+	for (size_t i = 0; i < _nrOfObjects; i++)
+		temp[i] = _gameObjects[i];
+	delete[] _gameObjects;
+	_gameObjects = temp;
+	initiate(_nrOfObjects);
+}
+
+void GameObjectHandler::initiate(int from)
+{
+	for (size_t i = from; i < _capacity; i++)
+		_gameObjects[i] = nullptr;
+}
+
+GameObjectHandler::GameObjectHandler(int capacity)
 {
 }
 
 GameObjectHandler::~GameObjectHandler()
 {
+	freeMemory();
+}
+
+GameObject * GameObjectHandler::getGameObject(int index) const
+{
+	return nullptr;
 }
--- Salvage/Salvage/GameObjectHandler.h
+++ Salvage/Salvage/GameObjectHandler.h
@@ -1,9 +1,19 @@
 #pragma once
+#include "GameObject.h"
 
 class GameObjectHandler
 {
 private:
+	int _capacity;
+	int _nrOfObjects;
+	GameObject** _gameObjects = nullptr;
+
+	void freeMemory();
+	void expand();
+	void initiate(int from = 0);
 public:
-	GameObjectHandler();
-	~GameObjectHandler();
+	GameObjectHandler(int capacity = 10);
+	virtual~GameObjectHandler();
+
+	GameObject* getGameObject(int index) const;
 };
